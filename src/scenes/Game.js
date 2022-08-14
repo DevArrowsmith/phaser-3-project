7b03cf87ad86adba4faa01bd1ae2ee149caa6250
DevArrowsmith <devarrowsmith@gmail.com>
2022-08-14 19:27:25 +0100

    Game scene: Add left paddle.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -8,9 +8,9 @@ export default class Game extends Phaser.Scene {
   create() {
     const ball = this.add.circle(400, 250, 10, 0xefefef, 1)
     this.physics.add.existing(ball)
-
     ball.body.setCollideWorldBounds(true, 1, 1)
-
     ball.body.setVelocity(-200, 200)
+
+    const paddleLeft = this.add.rectangle(30, 250, 16, 100, 0xefefef, 1)
   }
 }
