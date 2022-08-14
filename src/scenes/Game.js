18f9bfbf6ec40516346f13f0a859a5fc8446e67f
DevArrowsmith <devarrowsmith@gmail.com>
2022-08-14 19:23:36 +0100

    Game scene: Make ball bounce off game bounds, preserve velocity.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,6 +9,8 @@ export default class Game extends Phaser.Scene {
     const ball = this.add.circle(400, 250, 10, 0xefefef, 1)
     this.physics.add.existing(ball)
 
+    ball.body.setCollideWorldBounds(true, 1, 1)
+
     ball.body.setVelocity(-200, 200)
   }
 }
